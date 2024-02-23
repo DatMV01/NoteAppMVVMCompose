8330eba57e97201e2ccda45209ddcc908516cfc9
DatMV01 <datmv01@gmail.com>
2024-02-23 21:32:11 +0700

    Change code

--- app/src/main/java/com/example/noteappmvvmcompose/di/AppModule.kt
+++ app/src/main/java/com/example/noteappmvvmcompose/di/AppModule.kt
@@ -1,7 +1,6 @@
 package com.example.noteappmvvmcompose.di
 
 import android.app.Application
-import androidx.room.Room
 import com.example.noteappmvvmcompose.feature_note.data.data_source.NoteDatabase
 import com.example.noteappmvvmcompose.feature_note.data.repository.NoteRepositoryImpl
 import com.example.noteappmvvmcompose.feature_note.domain.repository.NoteRepository
@@ -24,11 +23,7 @@ object AppModule {
     @Provides
     @Singleton
     fun provideNoteDatabase(app: Application): NoteDatabase {
-        return Room.databaseBuilder(
-            app,
-            NoteDatabase::class.java,
-            NoteDatabase.DATABASE_NAME
-        ).build()
+        return NoteDatabase.getDatabase(app)
     }
 
     @Provides
